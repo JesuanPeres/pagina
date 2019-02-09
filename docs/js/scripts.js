9b4c942cad3d36887680ed6d91eb8e9a7eecd3af
Jesuan <jesuanperes@gmail.com>
2019-02-09 16:11:24 -0200

    Erro Corrigido

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -2,9 +2,9 @@ var btnRun = document.getElementById('btn-run');
 const TAM = window.innerWidth;
 var marg = 0;
 function run(){
-	btnRun.style = "margin-left:"+ marg +"px";
-	
 	if(marg < TAM - 200)
 		marg += 100;
 	else marg = 0;
+	
+	btnRun.style = "margin-left:"+ marg +"px";
 }
