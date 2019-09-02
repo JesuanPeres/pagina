132b776d7c1e3df496d32cf028e0d86a5d7bff86
Jesuan Peres <jesuanperes@gmail.com>
2019-09-02 00:23:38 -0300

    Adicionando colisão em y

--- docs/scripts/game.js
+++ docs/scripts/game.js
@@ -5,35 +5,43 @@ const FLOOR_HEIGHT = 10;
 
 window.onload = function() {
     Crafty.init(GAME_WIDTH, GAME_HEIGHT, document.getElementById('game'));
+    Crafty.background('#ffa');
 
 
     Crafty.c('character', {
         init: function(){
-            this.addComponent("2D, Canvas, Twoway, Gravity, Color")
+            this.addComponent("2D, Canvas, Twoway, Motion, Gravity, Color, Collision")
             .attr({
                 w: 50,
                 h: 50,
-            }).twoway(200).gravity('floor');
-
-            
+            }).twoway(200).gravity('floor').onHit('platform', function(){
+                this.y += 1;
+                this.velocity().y = 0;
+            });
         },
 
         posit: function(x, y){
             this.x = x;
             this.y = y;
         },
+
+
     })
 
     // //Adicionando o jogador
     let player = Crafty.e('character').color('#f00').posit(10, GAME_HEIGHT / 2);
 
+    // let player2 = Crafty.e('character').color('#00f').posit(200, GAME_HEIGHT /2);
+
     //Adicionando o chão
-    Crafty.e('floor, 2D, DOM, Color').attr({
+    Crafty.e('floor, 2D, DOM, Color, Solid').attr({
         x: 0,
         y: GAME_HEIGHT - 10,
         w: GAME_WIDTH / 2 - 100,
         h: FLOOR_HEIGHT,
     }).color('#aaa');
+
+
 
     Crafty.e('floor, 2D, DOM, Color').attr({
         x: GAME_WIDTH / 2,
@@ -42,12 +50,13 @@ window.onload = function() {
         h: FLOOR_HEIGHT,
     }).color('#aaa');
 
-    Crafty.e('floor, 2D, DOM, Color').attr({
+    Crafty.e('platform, 2D, DOM, Color, floor').attr({
         x: GAME_WIDTH / 2,
         y: GAME_HEIGHT / 2 - 10,
         w: 100,
         h: FLOOR_HEIGHT,
     }).color('#aaa');
 
+    Crafty.log(player);
     
 }
